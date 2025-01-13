3b0efb6b5ec9d0093ac2719c84a6f14a64622c23
Irene <12470297+cydia2020@users.noreply.github.com>
2025-01-14 10:17:35 +1100

    add DSU reroute support for UNSUPPORTED_DSU_CAR

--- opendbc/car/toyota/carstate.py
+++ opendbc/car/toyota/carstate.py
@@ -153,8 +153,8 @@ class CarState(CarStateBase):
 
     if self.CP.carFingerprint in UNSUPPORTED_DSU_CAR:
       # TODO: find the bit likely in DSU_CRUISE that describes an ACC fault. one may also exist in CLUTCH
-      ret.cruiseState.available = cp.vl["DSU_CRUISE"]["MAIN_ON"] != 0
-      ret.cruiseState.speed = cp.vl["DSU_CRUISE"]["SET_SPEED"] * CV.KPH_TO_MS
+      ret.cruiseState.available = cp_acc.vl["DSU_CRUISE"]["MAIN_ON"] != 0
+      ret.cruiseState.speed = cp_acc.vl["DSU_CRUISE"]["SET_SPEED"] * CV.KPH_TO_MS
     else:
       ret.accFaulted = cp.vl["PCM_CRUISE_2"]["ACC_FAULTED"] != 0
       ret.cruiseState.available = cp.vl["PCM_CRUISE_2"]["MAIN_ON"] != 0
@@ -241,9 +241,10 @@ class CarState(CarStateBase):
         ("GEAR_PACKET", 1),
       ]
 
-    if CP.carFingerprint in UNSUPPORTED_DSU_CAR:
+    if CP.carFingerprint in UNSUPPORTED_DSU_CAR and not CP.flags & ToyotaFlags.DISABLE_RADAR.value:
       pt_messages.append(("DSU_CRUISE", 5))
-      pt_messages.append(("PCM_CRUISE_ALT", 1))
+    elif CP.carFingerprint in UNSUPPORTED_DSU_CAR and CP.flags & ToyotaFlags.DISABLE_RADAR.value:
+      cam_messages.append(("DSU_CRUISE", 5))
     else:
       pt_messages.append(("PCM_CRUISE_2", 33))
 
