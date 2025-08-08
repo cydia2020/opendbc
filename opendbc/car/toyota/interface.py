8ee4ff96919f4f77b9140fcc5b91ef259f663925
Irene <12470297+cydia2020@users.noreply.github.com>
2025-08-08 17:16:05 +1000

    Revert "detect dsu on bus 2"
    This reverts commit d11774f0b0e66f2ef192b966e7814c5144660ab4.

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -54,10 +54,9 @@ class CarInterface(CarInterfaceBase):
     if any(msg in fingerprint[0] for msg in [0x23]):
       ret.flags |= ToyotaFlags.SECONDARY_STEER_ANGLE.value
 
-    # Detect DSU on bus 2
-    for fw in car_fw:
-      if fw.ecu == "dsu" and fw.bus == "2" and candidate not in TSS2_CAR:
-        ret.flags |= ToyotaFlags.DSU_BYPASS.value
+    # Detect 0x343 on bus 2, if detected on bus 2 and is not TSS 2, it means DSU is bypassed
+    if any(msg in fingerprint[2] for msg in (0x343, 0x4CB)) and candidate not in TSS2_CAR:
+      ret.flags |= ToyotaFlags.DSU_BYPASS.value
 
     # In TSS2 cars, the camera does long control
     found_ecus = [fw.ecu for fw in car_fw]
