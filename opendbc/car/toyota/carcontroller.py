43424b7866b3d8f62dcb74f470d10c165ce663a9
Irene <12470297+cydia2020@users.noreply.github.com>
2025-08-11 12:32:36 +1000

    keep dsu disabled

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -315,6 +315,10 @@ class CarController(CarControllerBase):
     if self.frame % 20 == 0 and self.CP.flags & ToyotaFlags.DISABLE_RADAR.value:
       can_sends.append(make_tester_present_msg(0x750, 0, 0xF))
 
+    # keep dsu disabled
+    if self.frame % 20 == 0 and self.CP.flags & ToyotaFlags.DISABLE_DSU.value:
+      can_sends.append(make_tester_present_msg(0x791, 0, None))
+
     new_actuators = actuators.as_builder()
     new_actuators.torque = apply_torque / self.params.STEER_MAX
     new_actuators.torqueOutputCan = apply_torque
