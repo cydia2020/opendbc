b718b0f79865afe65e22d4aa28710343d4decda4
Irene <12470297+cydia2020@users.noreply.github.com>
2025-08-11 12:31:57 +1000

    put dsu into bootloader

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -171,15 +171,20 @@ class CarInterface(CarInterfaceBase):
     return ret
 
   @staticmethod
-  def init(CP, can_recv, can_send, communication_control=None):
+  def init(CP, can_recv, can_send, communication_control=None, diagnostic_session_control=None):
     # disable radar if alpha longitudinal toggled on radar-ACC car
     if CP.flags & ToyotaFlags.DISABLE_RADAR.value:
       if communication_control is None:
         communication_control = bytes([uds.SERVICE_TYPE.COMMUNICATION_CONTROL, uds.CONTROL_TYPE.ENABLE_RX_DISABLE_TX, uds.MESSAGE_TYPE.NORMAL])
       disable_ecu(can_recv, can_send, bus=0, addr=0x750, sub_addr=0xf, com_cont_req=communication_control)
+    if CP.flags & ToyotaFlags.DISABLE_DSU.value:
+      if diagnostic_session_control is None:
+        diagnostic_session_control = bytes([uds.SERVICE_TYPE.DIAGNOSTIC_SESSION_CONTROL, uds.SESSION_TYPE.PROGRAMMING, uds.MESSAGE_TYPE.NORMAL])
+      disable_ecu(can_recv, can_send, bus=0, addr=0x791, sub_addr=None, com_cont_req=diagnostic_session_control)
 
   @staticmethod
   def deinit(CP, can_recv, can_send):
     # re-enable radar if alpha longitudinal toggled on radar-ACC car
     communication_control = bytes([uds.SERVICE_TYPE.COMMUNICATION_CONTROL, uds.CONTROL_TYPE.ENABLE_RX_ENABLE_TX, uds.MESSAGE_TYPE.NORMAL])
-    CarInterface.init(CP, can_recv, can_send, communication_control)
+    diagnostic_session_control = bytes([uds.SERVICE_TYPE.DIAGNOSTIC_SESSION_CONTROL, uds.SESSION_TYPE.DEFAULT, uds.MESSAGE_TYPE.NORMAL])
+    CarInterface.init(CP, can_recv, can_send, communication_control, diagnostic_session_control)
